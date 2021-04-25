136ce2b014273382ef90d6bd3ace36b8e79a6f69
malathi416 <ammu.416@gmail.com>
2021-04-25 08:28:26 -0500

    Initial commit

---
+++ src/test/java/com/launchcode/schoolprojectmanagementdemo/SchoolProjectManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchcode.schoolprojectmanagementdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProjectManagementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
